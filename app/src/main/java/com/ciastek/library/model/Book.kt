5e78023c2a6f02ed25202c1e1377c9c6256b36f2
Emilia_Ciastek <Emilia_Ciastek@epam.com>
2018-07-13 15:47:46 +0200

    Change equals and hashCode implementation for Book

--- app/src/main/java/com/ciastek/library/model/Book.kt
+++ app/src/main/java/com/ciastek/library/model/Book.kt
@@ -4,11 +4,17 @@ import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
+import java.util.*
 
 @Entity(tableName = "books")
 @Parcelize
 data class Book(
-        @PrimaryKey(autoGenerate = true) val id: Long? = null,
+        @PrimaryKey(autoGenerate = true) val bookId: Long? = null,
         val title: String,
         val author: String,
-        var isRead: Boolean = false) : Parcelable
+        var isRead: Boolean = false) : Parcelable {
+
+    override fun equals(other: Any?): Boolean = other is Book && other.bookId == bookId ?: false
+
+    override fun hashCode(): Int = Objects.hashCode(bookId)
+}
