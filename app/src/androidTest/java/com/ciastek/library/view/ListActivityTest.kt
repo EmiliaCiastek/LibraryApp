97d96eb722a168ae298eaa61852fda76cf0d9e5c
Emilia Ciastek <emilia.ciastek@farmaprom.pl>
2019-12-08 12:58:43 +0100

    Fix espresso test, upgrade emulator version

--- app/src/androidTest/java/com/ciastek/library/view/ListActivityTest.kt
+++ app/src/androidTest/java/com/ciastek/library/view/ListActivityTest.kt
@@ -1,11 +1,17 @@
 package com.ciastek.library.view
 
 import android.support.test.espresso.Espresso.onView
-import android.support.test.espresso.action.ViewActions.*
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.action.ViewActions.closeSoftKeyboard
+import android.support.test.espresso.action.ViewActions.typeText
 import android.support.test.espresso.assertion.ViewAssertions.doesNotExist
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.contrib.RecyclerViewActions
-import android.support.test.espresso.matcher.ViewMatchers.*
+import android.support.test.espresso.matcher.ViewMatchers.hasDescendant
+import android.support.test.espresso.matcher.ViewMatchers.isChecked
+import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.espresso.matcher.ViewMatchers.withText
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import com.ciastek.library.R
