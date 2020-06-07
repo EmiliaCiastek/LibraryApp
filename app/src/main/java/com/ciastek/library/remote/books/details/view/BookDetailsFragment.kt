345ace380e301316d77f0194131e373d1e11c483
Emilia Ciastek <emilia.ciastek@farmaprom.pl>
2020-06-07 11:46:36 +0200

    Add book details view with mocked data

--- app/src/main/java/com/ciastek/library/remote/books/details/view/BookDetailsFragment.kt
+++ app/src/main/java/com/ciastek/library/remote/books/details/view/BookDetailsFragment.kt
@@ -1,12 +1,16 @@
 package com.ciastek.library.remote.books.details.view
 
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.ciastek.library.R
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.book_details_fragment.*
+
 
 class BookDetailsFragment: Fragment() {
 
@@ -20,6 +24,12 @@ class BookDetailsFragment: Fragment() {
                               savedInstanceState: Bundle?): View? =
         inflater.inflate(R.layout.book_details_fragment, container, false)
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        textView5.movementMethod = ScrollingMovementMethod()
+    }
+
     override fun onStart() {
         super.onStart()
 
@@ -28,5 +38,9 @@ class BookDetailsFragment: Fragment() {
         Toast.makeText(context,
                        "Opened details for book with id: $id",
                        Toast.LENGTH_SHORT).show()
+
+        Picasso.get().load("https://tessgerritsenbooklist.com/wp-content/uploads/2014/01/Tess-Gerritsen-Gravity1.jpg")
+                .placeholder(R.drawable.cover_placeholder)
+                .into(imageView5)
     }
 }
