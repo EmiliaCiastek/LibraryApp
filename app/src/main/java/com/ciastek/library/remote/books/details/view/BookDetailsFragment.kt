04a8fcd9ee7c74ba5b9df4631988bd3505af99ff
Emilia Ciastek <emilia.ciastek@farmaprom.pl>
2020-06-12 16:39:48 +0200

    Change book details view

---
+++ app/src/main/java/com/ciastek/library/remote/books/details/view/BookDetailsFragment.kt
@@ -0,0 +1,31 @@
+package com.ciastek.library.remote.books.details.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ciastek.library.R
+
+class BookDetailsFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_book_details, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setHasOptionsMenu(true)
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+
+        inflater.inflate(R.menu.menu_book_details, menu)
+    }
+}
